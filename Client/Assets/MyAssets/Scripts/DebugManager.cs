1072fd019cd62370125fabaae848b17b59a2e213
Alankrit Shah <ashah28@users.noreply.github.com>
2017-09-15 18:01:08 -0700

    Added Client code

---
+++ Client/Assets/MyAssets/Scripts/DebugManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+
+public class DebugManager : Singleton<DebugManager> {
+
+    [SerializeField]
+    Text runningLog;
+
+    [SerializeField]
+    Text infoLog;
+    
+	// Use this for initialization
+	void Start () {
+        if (!runningLog)
+            Debug.LogError("Visual log not found");
+	}
+	
+    public void PrintToRunningLog(string message)
+    {
+        string data = GetCurrentTimestamp() + " : " + message + "\n";
+        print(data);
+        runningLog.text = data + runningLog.text.Substring(0, Mathf.Min(runningLog.text.Length, 1000));
+    }
+
+    public void PrintToInfoLog(string message)
+    {
+        string data = GetCurrentTimestamp() + " : " + message + "\n";
+        print(data);
+        infoLog.text = data + infoLog.text.Substring(0, Mathf.Min(infoLog.text.Length, 1000));
+    }
+
+    string GetCurrentTimestamp()
+    {
+        return Time.time.ToString("000.000");
+    }
+}
---
+++ Client/Assets/MyAssets/Scripts/ImageCapture.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.VR.WSA.WebCam;
+using System;
+using System.IO;
+
+public class ImageCapture : MonoBehaviour
+{
+    PhotoCapture photoCaptureObject = null;
+    Texture2D targetTexture = null;
+
+    [SerializeField] Renderer quadRendererCustom;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+
+        // Create a PhotoCapture object
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+            photoCaptureObject = captureObject;
+            CameraParameters cameraParameters = new CameraParameters();
+            cameraParameters.hologramOpacity = 0.0f;
+            cameraParameters.cameraResolutionWidth = cameraResolution.width;
+            cameraParameters.cameraResolutionHeight = cameraResolution.height;
+            cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+            // Activate the camera
+            photoCaptureObject.StartPhotoModeAsync(cameraParameters, delegate (PhotoCapture.PhotoCaptureResult result) {
+                DebugManager.Instance.PrintToRunningLog("Cam enabled");
+                photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            });
+        });
+    }
+
+    private void OnDisable()
+    {
+        photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
+        DebugManager.Instance.PrintToRunningLog("Cam disabled");
+    }
+
+    public void CaptureImage()
+    {
+        DebugManager.Instance.PrintToRunningLog("Capture");
+        photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+    }
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        // Copy the raw image data into the target texture
+        photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+
+        //Set texture to renderer. Probably need to do just once... WIP
+        quadRendererCustom.material.mainTexture = targetTexture;
+
+        try
+        {
+            byte[] imageData = targetTexture.EncodeToJPG(100);
+            WriteImageToDisk(imageData);
+        }
+        catch (Exception e)
+        {
+            DebugManager.Instance.PrintToInfoLog("Writing to disk failed:" + e.ToString());
+        }
+    }
+
+    void WriteImageToDisk(byte[] imageData)
+    {
+        String currentFileName = "Capture_" + " " + String.Format("{0:MM-dd,hh-mm-ss}", System.DateTime.Now);
+        String filePath = Application.persistentDataPath + "/" + currentFileName;
+        File.WriteAllBytes(filePath + ".jpg", imageData);
+        DebugManager.Instance.PrintToInfoLog("Saved:" + filePath);
+    }
+
+    void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
+    {
+        // Shutdown the photo capture resource
+        photoCaptureObject.Dispose();
+        photoCaptureObject = null;
+    }
+}
